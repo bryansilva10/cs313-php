f9e930d3d02669661db37cfa8b13ec8068fcdc02
Bryan Guillen <bryanguillen@Bryans-MacBook-Pro.local>
2019-10-15 22:20:32 -0600

    Added project folder and updated tables

---
+++ web/TODO LIST APP/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <div class="heading">
+            <h2 style="font-style: 'Helvetica';">ToDo List</h2>
+        </div>
+        <form method="post" action="action.php" class="input_form">
+            <input type="text" name="task" class="task_input">
+            <button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
+        </form>
+    </body>
+</html>
