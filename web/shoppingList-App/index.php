4ed4755eecc7fab4489867ff5db449b34a62795f
Bryan Guillen <bryanguillen@Bryans-MacBook-Pro.local>
2019-10-23 12:58:39 -0600

    Added new folders and files

--- web/shoppingList-App/index.php
+++ web/shoppingList-App/index.php
@@ -13,9 +13,9 @@
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
-  
+
   <body>
-    <div class="container">
+    <div class="container text-center">
         <div class="jumbotron">
             <h1>Family Shopping List</h1>
         </div>
